d38e8cf65c9ac1c4f0e63ef20b002b3a5de4ea97
Rudi Welter <rudi.welter@gmail.com>
2023-12-17 14:32:24 +0100

    added another method to controller

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 	@GetMapping("/")
 	public String sayHello() {
-		return ("Hello Rudi XXX");	
+		return ("Hello Rudi Again");	
 	}
 
 	@GetMapping("/test")
